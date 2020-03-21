145af129f4dd7954d39b2b18bb3f93617e895680
mchev <martin.chevignard@gmail.com>
2020-03-21 12:20:10 +0100

    websockets

--- config/websockets.php
+++ config/websockets.php
@@ -110,13 +110,13 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
+        'local_cert' => null,
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
+        'local_pk' => null,
 
         /*
          * Passphrase for your local_cert file.
