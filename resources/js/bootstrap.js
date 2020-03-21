9687def323bd11c685cb7d4e00fdcaaaadbf1742
mchev <martin.chevignard@gmail.com>
2020-03-21 13:03:28 +0100

    websocket

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -53,11 +53,9 @@ window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.PUSHER_APP_CLUSTER,
-    encrypted: true,
-    /*
     wsHost: window.location.hostname,
-    wsPort: 443,
-    wssPort: 443,
-    disableStats: true,*/
+    wsPort: 6001,
+    wssPort: 6001,
+    disableStats: true,
+    encrypted: true
 });
