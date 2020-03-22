579124b505290881b3805353c17ba9446a67f478
mchev <martin.chevignard@gmail.com>
2020-03-22 12:00:01 +0100

    Ads

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
-@section('title', 'Blind-Tests multijoueurs gratuits, sans pub et sans inscription')
-@section('description', 'Simple et efficace! Blind-tests multijoueurs, gratuit, sans pub et sans inscription. Disney, Séries, Films, Années 80, etc.')
+@section('title', 'Blind-Tests multijoueurs, gratuits et sans inscription')
+@section('description', 'Simple et efficace! Blind-tests multijoueurs, gratuit et sans inscription. Disney, Séries, Films, Années 80, etc.')
 
 @section('content')
 
