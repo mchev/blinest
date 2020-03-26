6758a7fe0ffee242bd814c7c6638661fe9d39505
mchev <martin.chevignard@gmail.com>
2020-03-26 18:30:06 +0100

    Remove icon home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,15 +11,6 @@
       <!-- Masthead Heading -->
       <h1 class="masthead-heading text-uppercase mb-0">Blind-Test</h1>
 
-      <!-- Icon Divider -->
-      <div class="divider-custom divider-light">
-        <div class="divider-custom-line"></div>
-        <div class="divider-custom-icon">
-          <i class="fas fa-headphones"></i>
-        </div>
-        <div class="divider-custom-line"></div>
-      </div>
-
       <!-- Masthead Subheading -->
       <p class="masthead-subheading font-weight-light mb-0">Testez votre culture musicale</p>
 
