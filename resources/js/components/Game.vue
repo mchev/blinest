0486d08a46358fa12f733e30ea20346df6f69490
mchev <martin.chevignard@gmail.com>
2020-03-28 17:56:44 +0100

    Fix design

--- resources/js/components/Game.vue
+++ resources/js/components/Game.vue
@@ -8,15 +8,6 @@
             <!-- Masthead Heading -->
             <h1 class="masthead-heading text-uppercase mb-0">Blind-Test {{ game.title }}</h1>
 
-            <!-- Icon Divider -->
-            <div class="divider-custom divider-light">
-              <div class="divider-custom-line"></div>
-              <div class="divider-custom-icon">
-                <i class="fas fa-star"></i>
-              </div>
-              <div class="divider-custom-line"></div>
-            </div>
-
             <p class="masthead-subheading font-weight-light mb-0">{{ game.description }}</p>
 
           </div>
