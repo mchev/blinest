e56fdb820836e4021cee9516eaa965f03a852b93
mchev <martin.chevignard@gmail.com>
2020-03-31 11:34:10 +0200

    Fix games delete when messages sended

---
+++ database/migrations/2020_03_31_112924_change_foreign_to_messages.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeForeignToMessages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropForeign(['game_id']);
+            $table->foreign('game_id')->references('id')->on('games')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            //
+        });
+    }
+}
