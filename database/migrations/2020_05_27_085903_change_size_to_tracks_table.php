1e357c573845fcb5f3a195b0ce105d507a7d34f1
mchev <martin.chevignard@gmail.com>
2020-05-27 09:03:52 +0200

    Fix Artist and Track size in db

---
+++ database/migrations/2020_05_27_085903_change_size_to_tracks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeSizeToTracksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tracks', function (Blueprint $table) {
+            $table->string('artist_name', 255)->change();
+            $table->string('track_name', 255)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tracks', function (Blueprint $table) {
+            //
+        });
+    }
+}
