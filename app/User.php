f4426809a57d6d39841aba52dd6e381238bd61be
mchev <martin.chevignard@gmail.com>
2020-11-24 20:12:23 +0100

    Reset password

--- app/User.php
+++ app/User.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable, CanResetPassword;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
