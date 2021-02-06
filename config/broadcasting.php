ba9db156258f3a7147ef82ab16258923223c8123
mchev <martin.chevignard@gmail.com>
2021-02-06 16:59:53 +0100

    Config pusher

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,6 +37,18 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => false,
+            ],
+        ],
+
+        /*
+        'pusher' => [
+            'driver' => 'pusher',
+            'key' => env('PUSHER_APP_KEY'),
+            'secret' => env('PUSHER_APP_SECRET'),
+            'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'https',
@@ -46,6 +58,7 @@ return [
                 ]
             ],
         ],
+        */
 
         'redis' => [
             'driver' => 'redis',
