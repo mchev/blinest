b0194a594001d2706c82aa8a305d4ac3b5784b02
mchev <martin.chevignard@gmail.com>
2021-03-11 20:54:22 +0100

    google-site-verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
 
     <script data-ad-client="ca-pub-6495635642797272" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <meta name="google-site-verification" content="e5HQfdzsH7U3iR8urwcghyw0uduK0IMasboHY53pRBs" />
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -43,14 +44,15 @@
     <script src="{{ mix('js/app.js') }}" defer></script>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id={{ env('GOOGLE_ANALYTICS_ID') }}"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-58430725-8"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
-      gtag('config', "{{ env('GOOGLE_ANALYTICS_ID') }}");
+      gtag('config', 'UA-58430725-8');
     </script>
+
 
     <!-- Prefetch -->
     <link rel="dns-prefetch" href="https://www.googletagmanager.com">
