0ccf3b058fa10facd6d05ae44ef82e61b07258f6
mchev <martin.chevignard@gmail.com>
2021-04-09 11:32:37 +0200

    Api cors

--- config/cors.php
+++ config/cors.php
@@ -21,7 +21,7 @@ return [
      * You can enable CORS for 1 or multiple paths.
      * Example: ['api/*']
      */
-    'paths' => [],
+    'paths' => ['api/*'],
 
     /*
     * Matches the request method. `[*]` allows all methods.
