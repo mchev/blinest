b50313b6936417783b24437459c56536eb2b79a7
mchev <martin.chevignard@gmail.com>
2021-04-09 11:31:03 +0200

    Updating laravel-cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,13 +14,13 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\PreferredDomain::class,
-        \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\RedirectIfAuthenticated::class,
     ];
 
