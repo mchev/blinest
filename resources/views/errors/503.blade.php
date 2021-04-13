3958674ed81bcbd402bfdf3d5192fa60b5347bc6
mchev <martin.chevignard@gmail.com>
2021-04-13 10:02:37 +0200

    503 blade view

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<title>Blinest - Site Maintenance</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>503</h1>
+    <h2>&#9749; Blinest est en train de devenir meilleur!</h2>
+    <div>
+      <p>Si vous voyez cette page c'est que quelqu'un est probablement en train de mettre en ligne de nouvelles fonctionnalités ou d'améliorer les performances du site.</p>
+      <p>Les coupures ne durent en général que quelques minutes.</p>
+        <p>&mdash; mchev</p>
+    </div>
+</article>
