83962e43a5d6ea915831ef6c2b997bf202813ed8
mchev <martin.chevignard@gmail.com>
2021-04-15 13:30:08 +0200

    Roman numerals to integer

--- resources/js/components/Games/Player.vue
+++ resources/js/components/Games/Player.vue
@@ -252,8 +252,29 @@
                     '1000' : 'mille'
                 };
 
+                var roman = {
+                    '1', 'I',
+                    '4', 'IV',
+                    '5', 'V',
+                    '9', 'IX',
+                    '10', 'X',
+                    '40', 'XL',
+                    '50', 'L',
+                    '90', 'XC',
+                    '100', 'C',
+                    '400', 'CD',
+                    '500', 'D',
+                    '900', 'CM', 
+                    '1000', 'M'
+                };
+
                 // Remove () and [] with content
                 str = str.replace(/ *\([^)]*\) */g, "").replace(/ *\[[^)]*\] */g, "");
+
+                // Replace roman numerals to integer
+                for (var pattern in roman) {
+                    str = str.replace(new RegExp(roman[pattern], 'g'), pattern);
+                };
 
                 // Lowercase
                 str = str.toLowerCase();
