59672afd6ed65069fe3e3a4e41dd3d3ff360c640
mchev <martin.chevignard@gmail.com>
2021-04-16 09:44:18 +0200

    Check before notify

--- app/Discord/Notification.php
+++ app/Discord/Notification.php
@@ -10,26 +10,31 @@ class Notification
 {
     public static function send(Game $game, $description, $color)
     {
-        if($color === 'danger') {
-            $color = '15158332';
-        } else if($color === 'success') {
-            $color = '3066993';
-        } else {
-            $color = '3447003';
+
+        if ( $game->discord_webhook_url || config('services.discord.webhook') ) {
+
+            if($color === 'danger') {
+                $color = '15158332';
+            } else if($color === 'success') {
+                $color = '3066993';
+            } else {
+                $color = '3447003';
+            }
+
+            $url = ($game->discord_webhook_url) ? $game->discord_webhook_url : config('services.discord.webhook');
+
+            return Http::post($url, [
+                //'content' => "Test bot blinest!",
+                'embeds' => [
+                    [
+                        'title' => $game->title,
+                        'description' => $description,
+                        'color' => $color,
+                    ]
+                ],
+            ]);
+
         }
-
-        $url = ($game->discord_webhook_url) ? $game->discord_webhook_url : config('services.discord.webhook');
-
-        return Http::post($url, [
-            //'content' => "Test bot blinest!",
-            'embeds' => [
-                [
-                    'title' => $game->title,
-                    'description' => $description,
-                    'color' => $color,
-                ]
-            ],
-        ]);
 
     }
 }
