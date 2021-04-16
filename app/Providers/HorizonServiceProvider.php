daf70b3b40f6986955e105154e4f5c14ccefd765
mchev <martin.chevignard@gmail.com>
2021-04-16 11:49:31 +0200

    Allow access to horizon dashboard for admins

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -34,9 +34,12 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewHorizon', function ($user) {
+            /*
             return in_array($user->email, [
                 //
             ]);
+            */
+            return $user->is('admin');
         });
     }
 }
