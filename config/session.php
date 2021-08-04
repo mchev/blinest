3c9b2c03796c25cf61c909e3ae3587e6b4476ddb
mchev <martin.chevignard@gmail.com>
2021-08-04 09:52:42 +0200

    Expire on close set to false

--- config/session.php
+++ config/session.php
@@ -33,7 +33,7 @@ return [
 
     'lifetime' => env('SESSION_LIFETIME', 120),
 
-    'expire_on_close' => true,
+    'expire_on_close' => false,
 
     /*
     |--------------------------------------------------------------------------
