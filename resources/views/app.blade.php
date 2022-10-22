7c866cb3f986a4875d25922c920e578e86630264
mchev <martin.chevignard@gmail.com>
2022-10-22 13:39:25 +0200

    Change tracker name for adblockers

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,8 +22,8 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300;400;600;700&display=swap" rel="stylesheet"> 
         
-        <script async defer data-website-id="f330893a-1490-47c0-8e3f-2e0fb4a88818" src="https://analytics.pegase.io/umami.js"></script>
-        
+        <script async defer data-website-id="f330893a-1490-47c0-8e3f-2e0fb4a88818" src="https://analytics.pegase.io/pegasestats.js"></script>
+
         @vite('resources/js/app.js')
         @routes
     
