bdc6e4e182175313999626ea32b6cde5f238f1f5
mchev <martin.chevignard@gmail.com>
2022-10-31 17:28:48 +0100

    Filtering scores

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -14,6 +14,7 @@ class RankingController extends Controller
     {
         return Inertia::render('Rankings/Index', [
             'bestUsers' => User::select('id', 'name')
+                ->whereHas('scores')
                 ->withSum(['scores as score' => function($query) {
                     $query->whereRelation('round', function($query) {
                         $query->whereRelation('room', function($q) {
@@ -24,6 +25,7 @@ class RankingController extends Controller
                 ->orderBy('score', 'DESC')
                 ->paginate(5),
             'bestTeams' => Team::select('id', 'name')
+                ->whereHas('scores')
                 ->withSum(['scores as score' => function($query) {
                     $query->whereRelation('round', function($query) {
                         $query->whereRelation('room', function($q) {
