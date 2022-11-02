761cca69ed62b70a7d67d4a459fd8162285c5a85
mchev <martin.chevignard@gmail.com>
2022-11-02 15:56:46 +0100

    Caching best scores for ten minutes

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -3,9 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Score;
-use App\Models\Room;
-use App\Models\Team;
-use App\Models\User;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
@@ -13,33 +11,38 @@ class RankingController extends Controller
 {
     public function index()
     {
+
+        $bestUsers = Cache::remember('bestUsers', now()->addMinutes(10), function () {
+            return Score::whereRelation('round', function($query) {
+                    $query->whereRelation('room', function($q) {
+                        $q->where('is_public', true);
+                    });
+                })
+                ->selectRaw("SUM(score) as total, user_id")
+                ->with('user')
+                ->groupBy('user_id')
+                ->orderBy('total', 'DESC')
+                ->paginate(5);
+        });
+
+        $bestTeams = Cache::remember('bestTeams', now()->addMinutes(10), function () {
+            return Score::whereNotNull('team_id')
+                ->whereRelation('round', function($query) {
+                    $query->whereRelation('room', function($q) {
+                        $q->where('is_public', true);
+                    });
+                })
+                ->selectRaw("SUM(score) as total, team_id")
+                ->with('team')
+                ->groupBy('team_id')
+                ->orderBy('total', 'DESC')
+                ->paginate(5);
+        });
         
         return Inertia::render('Rankings/Index', [
-            'bestUsers' => Score::whereRelation('round', function($query) {
-                        $query->whereRelation('room', function($q) {
-                            $q->where('is_public', true);
-                        });
-                    })
-                    ->selectRaw("SUM(score) as total, user_id")
-                    ->with('user')
-                    ->groupBy('user_id')
-                    ->orderBy('total', 'DESC')
-                    ->paginate(5),
-            'bestTeams' => Score::whereNotNull('team_id')
-                    ->whereRelation('round', function($query) {
-                        $query->whereRelation('room', function($q) {
-                            $q->where('is_public', true);
-                        });
-                    })
-                    ->selectRaw("SUM(score) as total, team_id")
-                    ->with('team')
-                    ->groupBy('team_id')
-                    ->orderBy('total', 'DESC')
-                    ->paginate(5)
-
+            'bestUsers' => $bestUsers,
+            'bestTeams' => $bestTeams
         ]);
-
-        dd($scores);
 
         // return Inertia::render('Rankings/Index', [
         //     'bestUsers' => User::select('id', 'name')
