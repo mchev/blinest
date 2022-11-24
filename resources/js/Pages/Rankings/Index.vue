6981e08b9ac11b9d50afbebbdfe42d318e2fdecc
mchev <martin.chevignard@gmail.com>
2022-11-24 11:01:29 +0100

    Link to the team in ranking page

--- resources/js/Pages/Rankings/Index.vue
+++ resources/js/Pages/Rankings/Index.vue
@@ -35,13 +35,15 @@ const props = defineProps({
           <section class="mb-4">
             <h3 class="mb-4 text-3xl font-bold">{{ __('Teams') }}</h3>
             <ul class="flex flex-wrap items-center justify-center">
-              <li v-for="(score, index) in bestTeams" :key="score.team.id" class="flex flex-col items-center m-4">
-                <div class="relative">
-                  <span class="p-1 bg-neutral-100 rounded-full h-8 w-8 flex items-center justify-center text-neutral-700 absolute -left-2 -top-2">{{ index + 1}}</span>
-                  <img :src="score.team.photo" class="mb-2 h-20 w-20 rounded-full" />
-                </div>
-                <span class="mb-1 font-bold">{{ score.team.name }}</span>
-                <span>{{ score.total_score }}<sup>PTS</sup></span>
+              <li v-for="(score, index) in bestTeams" :key="score.team.id">
+                <Link :href="route('teams.show', score.team.id)" class="flex flex-col items-center m-4">
+                  <div class="relative">
+                    <span class="p-1 bg-neutral-100 rounded-full h-8 w-8 flex items-center justify-center text-neutral-700 absolute -left-2 -top-2">{{ index + 1}}</span>
+                    <img :src="score.team.photo" class="mb-2 h-20 w-20 rounded-full" />
+                  </div>
+                  <span class="mb-1 font-bold">{{ score.team.name }}</span>
+                  <span>{{ score.total_score }}<sup>PTS</sup></span>
+                </Link>
               </li>
             </ul>
           </section>
