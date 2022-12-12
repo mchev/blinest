4eef2a3b41f447157838e142a6f4f51ef13d51cc
mchev <martin.chevignard@gmail.com>
2022-12-12 17:39:07 +0100

    Change George Banner

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -21,7 +21,7 @@ defineProps({
       <div class="flex flex-wrap items-center">
         <div class="lg:pr-8 py-8 mx-auto">
           <a href="http://www.georgequiz.com/?utm_source=blinest&utm_medium=banner&utm_campaign=dec2022" target="_blank" class="umami--click--george-banner" title="George Quiz">
-            <img src="/images/statics/george_V4.2.gif" loading="lazy" alt="George" class="h-full rounded-lg" />
+            <img src="/images/statics/george_V4.1.gif" loading="lazy" alt="George" class="h-full rounded-lg" />
           </a>
         </div>
         <div v-if="top_rooms" class="relative mb-4 flex-grow">
