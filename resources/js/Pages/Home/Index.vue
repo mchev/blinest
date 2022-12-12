ec0d56050466fb49f2c566307f774b202917c2c1
mchev <martin.chevignard@gmail.com>
2022-12-12 17:45:40 +0100

    Ad text

--- resources/js/Pages/Home/Index.vue
+++ resources/js/Pages/Home/Index.vue
@@ -19,9 +19,10 @@ defineProps({
   <Layout>
     <section v-if="!filters.search">
       <div class="flex flex-wrap items-center">
-        <div class="lg:pr-8 py-8 mx-auto">
+        <div class="lg:pr-8 py-6 mx-auto">
           <a href="http://www.georgequiz.com/?utm_source=blinest&utm_medium=banner&utm_campaign=dec2022" target="_blank" class="umami--click--george-banner" title="George Quiz">
             <img src="/images/statics/george_V4.1.gif" loading="lazy" alt="George" class="h-full rounded-lg" />
+            <small class="text-xs text-neutral-500">Lien sponsorisé - George Quiz</small>
           </a>
         </div>
         <div v-if="top_rooms" class="relative mb-4 flex-grow">
