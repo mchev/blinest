93d3db01a8b7033a4e069b8661bff5fca5e5ed94
mchev <martin.chevignard@gmail.com>
2023-02-08 14:26:17 +0100

    Inertia Heads and SSR gitignore

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,16 +3,20 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
         
         <title inertia>{{ config('app.name', 'Blinest') }}</title>
         <meta inertia name="description" content="Simple et efficace! Blind-tests multijoueurs, Années 2000, Disney, Chanson française, Années 80, etc.">
         <link rel="canonical" href="{{ URL::current() }}" />
         <link rel="icon" type="image/x-icon" href="/favicon.svg">
 
-        <meta property="og:locale" content="fr_FR"/>
-        <meta property="og:locale:alternate" content="en_US"/>
-
+        @production
+            <script async defer data-website-id="f330893a-1490-47c0-8e3f-2e0fb4a88818" src="https://analytics.pegase.io/pegasestats.js"></script>
+        @endproduction
+        
+        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @routes
+        @inertiaHead
+    
         <script type="application/ld+json">
         {
             "@context": "https://schema.org",
@@ -37,15 +41,6 @@
           }]
         }
         </script>
-
-
-        @production
-            <script async defer data-website-id="f330893a-1490-47c0-8e3f-2e0fb4a88818" src="https://analytics.pegase.io/pegasestats.js"></script>
-        @endproduction
-        
-        @vite('resources/js/app.js')
-        @routes
-    
 </head>
 <body class="font-sans antialiased bg-neutral-900">
     @inertia
