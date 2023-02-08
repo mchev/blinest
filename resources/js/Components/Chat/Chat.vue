a769450da739418d4d3b39ac566682ffeadbd2e7
mchev <martin.chevignard@gmail.com>
2023-02-09 00:41:37 +0100

    Better way to display alert button

--- resources/js/Components/Chat/Chat.vue
+++ resources/js/Components/Chat/Chat.vue
@@ -59,7 +59,7 @@ const scrollToBottom = () => {
 <template>
   <div class="flex h-full flex-col">
     <div class="flex items-center justify-center gap-2 px-2 py-4">
-      <button class="btn-secondary btn-sm bg-neutral-700" @click="alertingModerators = true" v-if="!reported">
+      <button class="btn-secondary btn-sm bg-neutral-700" @click="alertingModerators = true" v-if="!reported" :title="__('Report a problem on the chat')">
         <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
         </svg>
