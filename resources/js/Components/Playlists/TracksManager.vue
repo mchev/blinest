66b146d5df13463342fb916d1762a4907aef5984
Ckrapock <125498940+Ckrapock@users.noreply.github.com>
2023-02-17 08:06:14 +0000

    34(1 et 5) Traduction importer et suggestions

--- resources/js/Components/Playlists/TracksManager.vue
+++ resources/js/Components/Playlists/TracksManager.vue
@@ -146,7 +146,7 @@ const updateDificulty = (e, track) => {
           ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="mr-2 h-4 w-4">
             <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
           </svg>
-          Exporter
+           {{ __('Export') }}
         </a>
       </div>
     </template>
@@ -187,7 +187,7 @@ const updateDificulty = (e, track) => {
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="mr-2 h-5 w-5">
             <path fill-rule="evenodd" d="M12 2.25a.75.75 0 01.75.75v11.69l3.22-3.22a.75.75 0 111.06 1.06l-4.5 4.5a.75.75 0 01-1.06 0l-4.5-4.5a.75.75 0 111.06-1.06l3.22 3.22V3a.75.75 0 01.75-.75zm-9 13.5a.75.75 0 01.75.75v2.25a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5V16.5a.75.75 0 011.5 0v2.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V16.5a.75.75 0 01.75-.75z" clip-rule="evenodd" />
           </svg>
-          Importer une playlist
+          {{ __('Import Playlist') }}
         </button>
         <div class="flex items-center gap-2">
           <SelectInput v-model="form.paginate">
