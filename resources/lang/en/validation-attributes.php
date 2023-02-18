e15c09d326fae0ea5f246cf36a2477175c09b62d
Ckrapock <125498940+Ckrapock@users.noreply.github.com>
2023-02-18 08:18:23 +0000

    34(13):EN - ajout du fichier validation-attributes

---
+++ resources/lang/en/validation-attributes.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'attributes' => [
+        'address' => 'address',
+        'age' => 'age',
+        'available' => 'available',
+        'city' => 'city',
+        'content' => 'content',
+        'country' => 'country',
+        'current_password' => 'current password',
+        'date' => 'date',
+        'day' => 'day',
+        'description' => 'description',
+        'email' => 'email address',
+        'excerpt' => 'excerpt',
+        'first_name' => 'first name',
+        'gender' => 'gender',
+        'hour' => 'hour',
+        'last_name' => 'last name',
+        'minute' => 'minute',
+        'mobile' => 'mobile',
+        'month' => 'month',
+        'name' => 'name',
+        'password' => 'password',
+        'password_confirmation' => 'password confirmation',
+        'phone' => 'phone',
+        'second' => 'second',
+        'sex' => 'sex',
+        'size' => 'size',
+        'time' => 'time',
+        'title' => 'title',
+        'username' => 'username',
+        'year' => 'year',
+    ],
+];
+
+
