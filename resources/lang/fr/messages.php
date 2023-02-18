b68b2641d342a9a302e6babb960f8a1d1123bb9c
Ckrapock <125498940+Ckrapock@users.noreply.github.com>
2023-02-18 02:42:47 +0000

    34(12):nouvel ajustement

--- resources/lang/fr/messages.php
+++ resources/lang/fr/messages.php
@@ -2,12 +2,12 @@
 
 return [
 
-    'welcome_subject' => 'Welcom to Blinest',
-    'welcome_title' => 'Welcome :name',
-    'welcome_intro' => 'If you receive this email, it means that you have just created an account on blinest.com.',
-    'welcome_content' => 'Thank you for joining the largest blind tester community online. You can now create your own playlists, private rooms and join or create a team.',
-    'welcome_action' => 'Play on Blinest',
-    'salutations' => 'Martin from Blinest',
+    'welcome_subject' => 'Bienvenue sur Blinest',
+    'welcome_title' => 'Bienvenue :name',
+    'welcome_intro' => 'Si tu reçois cet e-mail, cela signifie que tu viens de créer un compte sur blinest.com.',
+    'welcome_content' => "Merci d'avoir rejoins la plus grande communauté de blind testeur et testeuse en ligne. Tu peux maintenant créer tes propres playlists, des parties privées et rejoindre ou créer une team.",
+    'welcome_action' => 'Jouer sur Blinest',
+    'salutations' => 'Martin de Blinest',
 
     'good' => [
         'Bien joué !',
