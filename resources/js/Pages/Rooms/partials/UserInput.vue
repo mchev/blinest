61bf374cdc58d1d73da3fdb8fa3f7c10dd54b47c
mchev <martin.chevignard@gmail.com>
2023-03-21 21:42:51 +0100

    #64 Fix user input contrast

--- resources/js/Pages/Rooms/partials/UserInput.vue
+++ resources/js/Pages/Rooms/partials/UserInput.vue
@@ -80,9 +80,9 @@ const showMessage = (data) => {
         <div class="absolute left-5 top-full mt-1 h-full h-0 w-full w-0 translate-y-[-50%] border-t-[10px] border-l-[10px] border-r-[10px] border-t-transparent border-l-transparent border-r-transparent" :class="{ 'border-t-teal-600': message.type === 'good', 'border-t-orange-600': message.type === 'almost', 'border-t-red-600': message.type === 'bad' }"></div>
       </blockquote>
 
-      <input ref="input" v-model="text" type="text" class="h-14 w-full flex-grow rounded-none rounded-bl-md border-none border-none p-2 text-2xl uppercase text-gray-600 focus:shadow-none focus:outline-none focus:ring-0" :placeholder="__('Any idea?')" autofocus :readonly="inputDisabled" />
+      <input ref="input" v-model="text" type="text" class="h-14 w-full bg-neutral-700 flex-grow rounded-none rounded-bl-md border-none border-none p-2 text-2xl uppercase focus:shadow-none focus:outline-none focus:ring-0" :placeholder="__('Any idea?')" autofocus :readonly="inputDisabled" />
 
-      <Volume class="-ml-1 flex h-14 items-center justify-center bg-white p-2 text-neutral-700" />
+      <Volume class="-ml-1 flex h-14 items-center justify-center bg-neutral-700 p-2" />
 
       <button type="submit" class="btn-send h-14">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="4" stroke="currentColor" class="h-6 w-6">
