0bb96ab77eb4e3b507266ad2f80a9076b929b8b0
mchev <martin.chevignard@gmail.com>
2023-03-23 15:59:39 +0100

    User input style and messages

--- resources/js/Pages/Rooms/partials/UserInput.vue
+++ resources/js/Pages/Rooms/partials/UserInput.vue
@@ -69,15 +69,14 @@ const showMessage = (data) => {
   message.value = data
   setTimeout(() => {
     message.value = null
-  }, 1500)
+  }, 1600)
 }
 </script>
 <template>
   <form class="flex w-full items-center justify-center" @submit.prevent="check">
     <div class="relative flex w-full items-center">
-      <blockquote v-if="message" class="absolute bottom-full right-0 flex translate-y-[-80%] translate-x-[-50%] items-center rounded-lg bg-teal-600 py-1 px-2 text-neutral-100" :class="{ 'bg-teal-600': message.type === 'good', 'bg-orange-600': message.type === 'almost', 'bg-red-600': message.type === 'bad' }">
+      <blockquote v-if="message" class="absolute top-full mt-2 right-0 opacity-80 flex rounded-lg py-1 px-2 text-neutral-100" :class="{ 'bg-teal-600': message.type === 'good', 'bg-orange-600': message.type === 'almost', 'bg-red-700': message.type === 'bad' }">
         {{ message.body }}
-        <div class="absolute left-5 top-full mt-1 h-full h-0 w-full w-0 translate-y-[-50%] border-t-[10px] border-l-[10px] border-r-[10px] border-t-transparent border-l-transparent border-r-transparent" :class="{ 'border-t-teal-600': message.type === 'good', 'border-t-orange-600': message.type === 'almost', 'border-t-red-600': message.type === 'bad' }"></div>
       </blockquote>
 
       <input ref="input" v-model="text" type="text" class="h-14 w-full bg-neutral-700 flex-grow rounded-none rounded-bl-md border-none border-none p-2 text-2xl uppercase focus:shadow-none focus:outline-none focus:ring-0" :placeholder="__('Any idea?')" autofocus :readonly="inputDisabled" />
