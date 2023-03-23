c40804c02919c256b4f0a36df91da73747d39134
mchev <martin.chevignard@gmail.com>
2023-03-23 21:15:33 +0100

    Fix Issue #67

--- resources/js/Components/SearchFilter.vue
+++ resources/js/Components/SearchFilter.vue
@@ -29,7 +29,7 @@ defineEmits(['update:modelValue', 'reset'])
           </div>
         </template>
       </dropdown>
-      <input class="focus:shadow-outline relative w-full rounded-r px-6 py-3 text-neutral-600" :class="{ 'rounded-l': !$slots.default }" autocomplete="off" type="text" name="search" :placeholder="__('Search…')" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" />
+      <input class="focus:shadow-outline relative w-full rounded-r px-6 py-3" :class="{ 'rounded-l': !$slots.default }" autocomplete="off" type="text" name="search" :placeholder="__('Search…')" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" />
     </div>
     <button class="ml-3 text-sm" type="button" @click="$emit('reset')">{{ __('Reset') }}</button>
   </div>
