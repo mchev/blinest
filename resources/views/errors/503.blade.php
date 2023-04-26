eea3dd0a1c9a6cc15de8d49955debb2eb6c3d929
mchev <martin.chevignard@gmail.com>
2023-04-26 09:48:00 +0200

    Maintenance page

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>Blinest fait une courte pause.</h1>
+    <div>
+        <p>
+            Le site est actuellement en cours de maintenance, il sera bientôt de nouveau accessible.<br>
+            Si besoin vous pouvez toujours <a href="mailto:hello@blinest.com">envoyer un mail</a>.
+        </p>
+        <p>&mdash; Martin de Blinest</p>
+    </div>
+</article>
