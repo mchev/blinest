e1308851577dce9544b90e8b0539b02d935308b4
mchev <martin.chevignard@gmail.com>
2023-05-04 14:43:35 +0200

    Authorize admin to update pictures

--- app/Providers/AuthorizationServiceProvider.php
+++ app/Providers/AuthorizationServiceProvider.php
@@ -10,10 +10,10 @@ class AuthorizationServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::define('changeRoomPicture', function () {
-            return auth()->user()->created_at < now()->subMonths(3) && floatval(auth()->user()->totalScores()->sum('score')) > 2000;
+            return auth()->user()->isAdministrator() || auth()->user()->created_at < now()->subMonths(3) && floatval(auth()->user()->totalScores()->sum('score')) > 2000;
         });
         Gate::define('changeTeamPicture', function () {
-            return auth()->user()->created_at < now()->subMonths(3) && floatval(auth()->user()->totalScores()->sum('score')) > 2000;
+            return auth()->user()->isAdministrator() || auth()->user()->created_at < now()->subMonths(3) && floatval(auth()->user()->totalScores()->sum('score')) > 2000;
         });
     }
 }
