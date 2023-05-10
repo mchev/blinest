c180d8735c616b3867d384d5b697deb69ab03c03
mchev <martin.chevignard@gmail.com>
2023-05-10 12:57:01 +0200

    Adding Net Zero Label

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -7,7 +7,7 @@ import { Link, usePage } from '@inertiajs/vue3'
   <div class="mt-8 border-t border-neutral-500 text-sm text-neutral-500 md:flex md:flex-shrink-0">
     <div class="flex w-full items-center justify-between py-2 md:flex-shrink-0">
       Blinest
-      <ul class="flex flex-col gap-4 md:flex-row">
+      <ul class="flex flex-col items-center gap-4 md:flex-row">
         <li><Link href="/contact">{{ __('Contact') }}</Link></li>
         <li>
           <a target="_blank" rel="external nofollow" href="https://github.com/mchev/blinest/issues/new">{{ __('Report a bug') }}</a>
@@ -15,6 +15,11 @@ import { Link, usePage } from '@inertiajs/vue3'
         <li><Link :href="route('pages.show', 'mentions-legales')">{{ __('Terms and Conditions') }}</Link></li>
         <li><Link :href="route('pages.show', 'politique-de-confidentialite')">{{ __('Privacy Policy') }}</Link></li>
         <li><a target="_blank" class="font-bold" rel="external nofollow" href="https://donate.stripe.com/00g2bvf8i08X8De6oo">{{ __('Donate') }}</a></li>
+        <li>
+          <a href="https://tree-nation.com/fr/profil/impact/blinest#co2" target="_blank" style="position:relative;cursor:pointer;display:block;z-index:999;">
+          <img src="https://tree-nation.com/images/tracking/label-co2-website-white-en.png" style="width:157px;height:auto;">
+          </a>
+        </li>
       </ul>
     </div>
   </div>
