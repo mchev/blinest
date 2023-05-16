048b3e1952d6e0ae2121e24a482170268aed3a33
mchev <martin.chevignard@gmail.com>
2023-05-16 17:41:02 +0200

    Temp bg color

--- resources/js/Pages/Home/partials/Room.vue
+++ resources/js/Pages/Home/partials/Room.vue
@@ -61,7 +61,7 @@ onUnmounted(() => {
       <div v-if="!room.is_public && room.owner" class="bg-purple-600 absolute truncate z-10 origin-top w-52 right-6 top-6 shadow-xl text-xs p-1 text-center" style="transform:translateX(50%) rotate(45deg);" >
         {{ room.owner.name }}
       </div>
-      <figure class="h-40 w-full relative rounded flex justify-center bg-orange-300">
+      <figure class="h-40 w-full relative rounded flex justify-center bg-teal-600">
         <img :src="room.photo" :alt="'Illlustration de la room ' + room.name" class="object-cover w-full h-full">
         <div class="absolute top-0 left-0 w-auto rounded-br rounded-tl-sm  p-2 text-sm text-black">
           <div class="flex items-center">
