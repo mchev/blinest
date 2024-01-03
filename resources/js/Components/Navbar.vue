9869ed5d491d525af83002edf4fb35b5323de711
mchev <martin.chevignard@gmail.com>
2024-01-03 17:18:48 +0100

    No more Xmas hat

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -19,14 +19,14 @@ const showingSearchbar = ref(false)
   <div class="md:flex md:flex-shrink-0">
     <div class="flex w-full items-center justify-between px-4 py-2 md:flex-shrink-0 md:px-12">
       <Link :href="route('home')" title="Blinest">
-        <logo class="w-24 fill-inherit lg:w-32" name="xmas" />
-        <p class="text-sm text-neutral-500 mt-1 hidden md:block">{{ __('Test your musical knowledge') }}</p>
+        <Logo class="w-24 fill-inherit lg:w-32" />
+        <p class="mt-1 hidden text-sm text-neutral-500 md:block">{{ __('Test your musical knowledge') }}</p>
       </Link>
 
-      <div class="mt-1 items-center gap-2 hidden md:flex">
-        <SearchRooms class="hover:scale-[104%] focus:scale-[104%] transition" />
-        <Link :href="route('faq')" title="FAQ" class="text-neutral-500 hover:text-inherit transition">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+      <div class="mt-1 hidden items-center gap-2 md:flex">
+        <SearchRooms class="transition hover:scale-[104%] focus:scale-[104%]" />
+        <Link :href="route('faq')" title="FAQ" class="text-neutral-500 transition hover:text-inherit">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-6 w-6">
             <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm11.378-3.917c-.89-.777-2.366-.777-3.255 0a.75.75 0 01-.988-1.129c1.454-1.272 3.776-1.272 5.23 0 1.513 1.324 1.513 3.518 0 4.842a3.75 3.75 0 01-.837.552c-.676.328-1.028.774-1.028 1.152v.75a.75.75 0 01-1.5 0v-.75c0-1.279 1.06-2.107 1.875-2.502.182-.088.351-.199.503-.331.83-.727.83-1.857 0-2.584zM12 18a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" />
           </svg>
         </Link>
@@ -34,7 +34,7 @@ const showingSearchbar = ref(false)
 
       <div class="flex items-center justify-end gap-4">
         <a href="https://shop.blinest.com" :title="__('Blinest Shop')" class="text-green-500" data-umami-event="Shop button">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
           </svg>
         </a>
