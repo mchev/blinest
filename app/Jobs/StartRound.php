5f48bae1387409c45e36eb3e4c841ad0ece38afc
mchev <martin.chevignard@gmail.com>
2024-03-24 15:43:34 +0100

    Fix mutliple rounds starts with unique jobs

--- app/Jobs/StartRound.php
+++ app/Jobs/StartRound.php
@@ -7,6 +7,7 @@ use App\Models\Room;
 use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
@@ -17,11 +18,26 @@ class StartRound implements ShouldQueue
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
+     * The number of seconds after which the job's unique lock will be released.
+     *
+     * @var int
+     */
+    public $uniqueFor = 60;
+
+    /**
      * Create a new job instance.
      */
     public function __construct(protected Room $room, protected ?User $user = null)
     {
         //
+    }
+
+    /**
+     * Get the unique ID for the job.
+     */
+    public function uniqueId(): string
+    {
+        return $this->room->id;
     }
 
     /**
