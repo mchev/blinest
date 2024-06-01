1ffee68d035abca0b46b66f70e7ba78cbdd61459
Shift <shift@laravelshift.com>
2024-06-01 04:27:48 +0000

    Adopt anonymous migrations

--- database/migrations/2021_12_06_155831_create_track_answers_table.php
+++ database/migrations/2021_12_06_155831_create_track_answers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTrackAnswersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -32,4 +32,4 @@ class CreateTrackAnswersTable extends Migration
     {
         Schema::dropIfExists('track_answers');
     }
-}
+};
