e31be33db5d0ac2ffb83654c62ece23c1e8a1c69
Shift <shift@laravelshift.com>
2024-06-01 04:27:43 +0000

    Re-register service providers

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\AuthorizationServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\MailServiceProvider::class,
 ];
--- config/app.php
+++ config/app.php
@@ -7,27 +7,5 @@ return [
 
     'admin_email' => env('ADMIN_EMAIL', null),
 
-    'providers' => ServiceProvider::defaultProviders()->merge([
-        /*
-         * Laravel Framework Service Providers...
-         */
-
-        /*
-         * Package Service Providers...
-         */
-        Intervention\Image\ImageServiceProvider::class,
-
-        /*
-         * Application Service Providers...
-         */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthorizationServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
-        App\Providers\MailServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-    ])->toArray(),
 
 ];
