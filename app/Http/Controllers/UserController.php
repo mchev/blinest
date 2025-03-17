328c3f43ff65e7e4959384cc577f69e18855a6ee
Martin Chevignard <martin.chevignard@gmail.com>
2025-03-17 17:20:48 +0100

    Upgrading to Laravel 12

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -65,10 +65,10 @@ class UserController extends Controller
     {
         if (Auth::user()->id === $user->id) {
             $request->validate([
-                'name' => ['required', 'max:25', Rule::unique('users')->ignore($user->id), new Reserved],
+                'name' => ['required', 'min:2', 'max:25', Rule::unique('users')->ignore($user->id), new Reserved],
                 'email' => ['required', 'max:255', 'email:rfc,dns', Rule::unique('users')->ignore($user->id)],
                 'password' => ['nullable', Rules\Password::defaults()],
-                'photo' => ['nullable', 'image'],
+                'photo' => ['nullable', 'image', 'max:1024'],
             ]);
 
             $user->update($request->only('name', 'email'));
